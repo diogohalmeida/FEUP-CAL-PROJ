56c70b1006aef46018b2bd155887ee5e1b1d5893
Diogo Almeida <diogohenriquealmeida@hotmail.com>
2020-05-25 02:13:39 +0100

    Added function to generate algorithm processs time graph

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "src/Interface/menu.h"
 
 
-
+void generateGraphDijsktra(vector<Graph<coordinates>> &regions);
 
 int main() {
     cout << "Hello There! Welcome to ExploreIT!" << endl;
@@ -15,8 +15,46 @@ int main() {
     while (mainMenu(regions) != 0);
 
 
+
+    //generateGraphDijsktra(regions);
+
+
     cout << "Thank you for using ExploreIT! Have a great tour!" << endl;
 
 
 	return 0;
+}
+
+void generateGraphDijsktra(vector<Graph<coordinates>> &regions) {
+    srand(time(NULL));
+    ofstream outputFile;
+    string fileName = "./graph.csv";
+    outputFile.open(fileName, ios::app);
+    int j = 1000;
+    for (int n = 1; n <= 10; n ++) {
+        // Start the clock for this n
+        auto start = chrono::high_resolution_clock::now();
+        // Create 1000 sequences
+        for (int i = 0; i < j; i++) {
+            int start_id = rand() % regions.at(0).getVertexSet().size();
+            if (regions.at(0).findVertex(start_id) == nullptr){
+                continue;
+            }
+            int end = rand() % regions.at(0).getVertexSet().size();
+            if (regions.at(0).findVertex(end) == nullptr){
+                continue;
+            }
+            regions.at(0).dijkstraShortestPath(start_id, end);
+        }
+
+        // Stop the clock
+        auto finish = chrono::high_resolution_clock::now();
+        // Get the total time
+        auto mili = chrono::duration_cast<chrono::milliseconds>(finish - start).count();
+        // Print to the .csv file
+        cout << j << " -> " << mili << endl;
+        outputFile << j << "," << mili << endl;
+        j += 200;
+    }
+    outputFile.close();
 }
